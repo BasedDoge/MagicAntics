52d4ca557d528caf0d08850b594336f99cae25b9
Isaac <Isaac_frechette1@hotmail.com>
2018-03-05 12:19:16 +1000

    fangs now return items to players

--- src/net/undergroundantics/magicantics/spells/Fangs.java
+++ src/net/undergroundantics/magicantics/spells/Fangs.java
@@ -1,10 +1,15 @@
 package net.undergroundantics.magicantics.spells;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.EvokerFangs;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.plugin.Plugin;
 
 public class Fangs implements Spell {
 
@@ -12,6 +17,10 @@ public class Fangs implements Spell {
     private static final String DISPLAY_NAME = "&7Fangs";
     private static final long COOLDOWN = 5;
 
+    public Fangs(Plugin plugin) {
+        this.plugin = plugin;
+    }
+    
     @Override
     public String getName() {
         return NAME;
@@ -29,6 +38,15 @@ public class Fangs implements Spell {
             l.setY(l.getWorld().getHighestBlockAt(l).getY());
             if (l.getY() >= p.getLocation().getY() - 4 && l.getY() <= p.getLocation().getY() + 4) {
                 EvokerFangs f = (EvokerFangs) p.getWorld().spawnEntity(l, EntityType.EVOKER_FANGS);
+                Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
+                    for (Entity ent : f.getNearbyEntities(0.5, 0.5, 0.5)){
+                        if(ent instanceof Item){
+                            ent.teleport(p);
+                            p.getWorld().spawnParticle(Particle.SPIT, p.getLocation(), 1, 0.2, 0.2, 0.2, 0.1);
+                        }
+                    }
+                f.remove();
+        }, 15);
             }
         }
         return COOLDOWN;
@@ -37,5 +55,5 @@ public class Fangs implements Spell {
     @Override
     public void onHit(ProjectileHitEvent e) {
     }
-
+    private final Plugin plugin;
 }
