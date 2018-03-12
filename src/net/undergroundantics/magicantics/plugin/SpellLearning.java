6c9b0847af85bd4fa8e7925fb4880970c6c12fb2
Isaac <Isaac_frechette1@hotmail.com>
2018-03-12 17:32:48 +1000

    Check to make sure event only fires once

--- src/net/undergroundantics/magicantics/plugin/SpellLearning.java
+++ src/net/undergroundantics/magicantics/plugin/SpellLearning.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public class SpellLearning implements Listener {
@@ -22,22 +23,24 @@ public class SpellLearning implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void learnSpellBook(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            ItemStack spellBook = e.getPlayer().getInventory().getItemInMainHand();
-            if (ItemRules.isSpellBook(spellBook)) {
-                e.setCancelled(true);
-                Spell spell = plugin.getSpellFromSpellBook(spellBook);
-                Player p = e.getPlayer();
-                if ( ! plugin.hasLearntSpell(p, spell) ) {
-                    plugin.learnSpell(p, spell);
-                    spellBook.setAmount(spellBook.getAmount() - 1);
-                    p.sendTitle(ChatColor.ITALIC + "" + ChatColor.GRAY + "New Spell Unlocked:", spell.getDisplayName(), 10, 40, 20);
-                    p.getWorld().playSound(e.getPlayer().getLocation(), Sound.ENTITY_WITHER_SPAWN, SoundCategory.PLAYERS, 0.5f, 0.0f);
-                    p.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, p.getEyeLocation(), 50, 0.25, 0.25, 0.25, 1);
-                } else {
-                    MagicAntics.sendMessage(p, ChatColor.GRAY + "You already know this spell.");
-                }
+        if (e.getHand() == EquipmentSlot.HAND) {
+            if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                ItemStack spellBook = e.getPlayer().getInventory().getItemInMainHand();
+                if (ItemRules.isSpellBook(spellBook)) {
+                    e.setCancelled(true);
+                    Spell spell = plugin.getSpellFromSpellBook(spellBook);
+                    Player p = e.getPlayer();
+                    if (!plugin.hasLearntSpell(p, spell)) {
+                        plugin.learnSpell(p, spell);
+                        spellBook.setAmount(spellBook.getAmount() - 1);
+                        p.sendTitle(ChatColor.ITALIC + "" + ChatColor.GRAY + "New Spell Unlocked:", spell.getDisplayName(), 10, 40, 20);
+                        p.getWorld().playSound(e.getPlayer().getLocation(), Sound.ENTITY_WITHER_SPAWN, SoundCategory.PLAYERS, 0.5f, 0.0f);
+                        p.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, p.getEyeLocation(), 50, 0.25, 0.25, 0.25, 1);
+                    } else {
+                        MagicAntics.sendMessage(p, ChatColor.GRAY + "You already know this spell.");
+                    }
 
+                }
             }
         }
     }
