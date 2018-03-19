e9524706d97db5795b91094b65c136abf321eeb4
Isaac <Isaac_frechette1@hotmail.com>
2018-03-19 19:28:27 +1000

    Fissure -->Singularity

--- src/net/undergroundantics/magicantics/plugin/MagicAntics.java
+++ src/net/undergroundantics/magicantics/plugin/MagicAntics.java
@@ -37,7 +37,7 @@ public class MagicAntics extends JavaPlugin {
         spells = new Spell[] { new Fangs(this), new Fireball(), new Icicle(this),
                  new Inferno(), new Stasis(this), new Thunderstorm(),
                  new Familiar(this), new Vindication(this), new Atomiser(),
-                 new Phase(), new Fissure(this)};
+                 new Phase(), new Singularity(this)};
     }
     
     public List<Spell> getSpells() {
