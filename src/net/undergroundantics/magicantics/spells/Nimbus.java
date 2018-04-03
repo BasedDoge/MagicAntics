b5ecbd7a540e8a5ec4da600f2a15a8ea16f60e36
BasedDoge <Isaacfrechette7@gmail.com>
2018-04-03 16:30:35 +1000

    Reworked nimbus  - now follows player  - happens every 1/4th of a second  - player can move down a block by sneaking

--- src/net/undergroundantics/magicantics/spells/Nimbus.java
+++ src/net/undergroundantics/magicantics/spells/Nimbus.java
@@ -14,6 +14,7 @@ public class Nimbus implements Spell {
     private static final String NAME = "Nimbus";
     private static final String DISPLAY_NAME = ChatColor.AQUA + NAME;
     private static final long COOLDOWN = 12;
+    private static final int DURATION = 12; //in quarters of seconds
     private static final int CLOUD_RADIUS = 2;
 
     public Nimbus(Plugin plugin) {
@@ -42,23 +43,34 @@ public class Nimbus implements Spell {
 
     @Override
     public boolean cast(Player p) {
-        p.setFallDistance(-1);
         Location cloudSeed = p.getLocation().subtract(CLOUD_RADIUS / 2, 1, CLOUD_RADIUS / 2);
-         for (int z = -CLOUD_RADIUS; z <= CLOUD_RADIUS; z++) {
+        for (int i = -DURATION; i <= DURATION; i++) {
+            Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
+                if (p.isSneaking()) {
+                    cloudSeed.subtract(0, 1, 1);
+                }
+                p.setFallDistance(-1);
+                buildCloud(cloudSeed, p.getLocation());
+            }, i * 5);
+        }
+        return true;
+    }
+
+    private void buildCloud(Location cloudSeed, Location playerLoc) {
+        for (int z = -CLOUD_RADIUS; z <= CLOUD_RADIUS; z++) {
             for (int x = -CLOUD_RADIUS; x <= CLOUD_RADIUS; x++) {
-                Location cloud = new Location(cloudSeed.getWorld(), p.getLocation().getX() + x, p.getLocation().getY() - 1, p.getLocation().getZ() + z);
-                if ( Math.pow(cloud.getX() - p.getLocation().getX(), 2) + Math.pow(cloud.getZ() - p.getLocation().getZ(), 2) <= Math.pow(CLOUD_RADIUS, 2) ) {
+                Location cloud = new Location(cloudSeed.getWorld(), playerLoc.getX() + x, playerLoc.getY() - 1, playerLoc.getZ() + z);
+                if (Math.pow(cloud.getX() - playerLoc.getX(), 2) + Math.pow(cloud.getZ() - playerLoc.getZ(), 2) <= Math.pow(CLOUD_RADIUS, 2)) {
                     if (cloud.getBlock().getType() == Material.AIR) {
                         cloud.getBlock().setType(Material.STAINED_GLASS);
                         Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
                             cloud.getBlock().setType(Material.AIR);
                             cloud.getWorld().spawnParticle(Particle.CLOUD, cloud, 4, 0.25, 0.25, 0.25, 0.2);
-                        }, 60);
+                        }, 30);
                     }
                 }
             }
         }
-        return true;
     }
     private final Plugin plugin;
 }
