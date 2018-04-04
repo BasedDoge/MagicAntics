7d2f2f08e97a77abb559a1be4e4324b9a2c16934
BasedDoge <Isaacfrechette7@gmail.com>
2018-04-04 12:29:41 +1000

    Added tab completion for spellknowledge

--- src/net/undergroundantics/magicantics/commands/CommandTabComplete.java
+++ src/net/undergroundantics/magicantics/commands/CommandTabComplete.java
@@ -1,5 +1,6 @@
 package net.undergroundantics.magicantics.commands;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import org.bukkit.command.Command;
@@ -12,7 +13,8 @@ import org.bukkit.entity.Player;
 public class CommandTabComplete implements TabCompleter {
 
     private MagicAntics plugin;
-    
+    private static List<String> kArgs = Arrays.asList("Add", "Clear", "Remove");
+
     public CommandTabComplete(MagicAntics plugin) {
         this.plugin = plugin;
     }
@@ -20,37 +22,58 @@ public class CommandTabComplete implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String string, String[] args) {
         if (cmd.getName().equalsIgnoreCase("spellscroll") || cmd.getName().equalsIgnoreCase("spellbook")) {
-            if ( args.length == 1 ) {
+            if (args.length == 1) {
                 return spellNames(args[0]);
-            } else if ( args.length == 2 ) {
+            } else if (args.length == 2) {
                 return playerNames(args[1]);
             }
             List<String> spells = new LinkedList<>();
         } else if (cmd.getName().equalsIgnoreCase("spelltome")) {
-            if ( args.length == 1 ) {
+            if (args.length == 1) {
                 return playerNames(args[0]);
             }
             
+        } else if (cmd.getName().equalsIgnoreCase("spellknowledge")) {
+            if (args.length == 1) {
+                return playerNames(args[0]);
+            } else if (args.length == 2) {
+                return knowledgeArgs(args[1]);
+            } else if (args.length == 3) {
+                return spellNames(args[2]);
+            }
         }
-        return null;        
+        return null;
     }
 
     private List<String> spellNames(String prefix) {
         List<String> spells = new LinkedList<>();
-        for (Spell spell : plugin.getLearnableSpells())
-            if (spell.getName().toLowerCase().startsWith(prefix.toLowerCase())){
+        for (Spell spell : plugin.getLearnableSpells()) {
+            if (spell.getName().toLowerCase().startsWith(prefix.toLowerCase())) {
                 spells.add(spell.getName());
             }
+        }
         return spells;
     }
-    
+
     private List<String> playerNames(String prefix) {
         List<String> players = new LinkedList<>();
-        for (Player player : plugin.getServer().getOnlinePlayers() )
-            if (player.getName().toLowerCase().startsWith(prefix.toLowerCase())){
+        for (Player player : plugin.getServer().getOnlinePlayers()) {
+            if (player.getName().toLowerCase().startsWith(prefix.toLowerCase())) {
                 players.add(player.getName());
             }
+        }
         return players;
+    }
+    
+    private List<String> knowledgeArgs(String prefix){
+        
+        List<String> args = new LinkedList<>();
+        for (String s : kArgs){
+            if (s.toLowerCase().startsWith(prefix.toLowerCase())) {
+                args.add(s);
+            }
+        }
+    return args;
     }
 
 }
